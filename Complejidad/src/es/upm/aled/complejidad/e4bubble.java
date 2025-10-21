e4eac8e6ed5571c645592f1295b0d8a5f8cd7f3d
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-10-21 19:49:21 +0200

    d

--- Complejidad/src/es/upm/aled/complejidad/e4bubble.java
+++ Complejidad/src/es/upm/aled/complejidad/e4bubble.java
@@ -16,8 +16,10 @@ public class e4bubble {
 		int n = arr.length;
 		for(int i = 0;i<n-1;i++) {
 			for(int j = 0;j<n-1-i;j++) {
-				if(arr[i]>arr[j]) {
-					arr[j]=arr[i];
+				if(arr[i]>arr[j+1]) {
+					int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 				}
 				}
 					
