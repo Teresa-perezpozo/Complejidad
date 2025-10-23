bd063e1cca46f919d843216fcde27d170297f5b3
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-10-23 09:38:33 +0200

    h

---
+++ Complejidad/src/es/upm/aled/complejidad/Nodo.java
@@ -0,0 +1,15 @@
+package es.upm.aled.complejidad;
+
+public class Nodo {
+	protected int clave;
+    protected int valor;
+    protected Nodo iz;
+    protected Nodo der;
+    
+    
+    
+	public static void main(String[] args) {
+
+	}
+
+}
