60914c48b545bd4addd5259a7db64e29769d4e83
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-11-16 18:51:17 +0100

    m

--- Complejidad/src/es/upm/aled/complejidad/LectorEscritor2_2.java
+++ Complejidad/src/es/upm/aled/complejidad/LectorEscritor2_2.java
@@ -1,14 +1,20 @@
 package es.upm.aled.complejidad;
 
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class LectorEscritor2_2 {
 private int valor1 =0;
 private int valor2=0;
-private final ReentrantReadWeiteLock rwLock = new ReentrantReanWeiteLock();
-private final ReentrantReadWeiteLock rwLock = new ReentrantReanWeiteLock();
+
+private final ReentrantReadWriteLock rwLock1 = new ReentrantReadWriteLock(); // Para valor1
+private final ReentrantReadWriteLock rwLock2 = new ReentrantReadWriteLock(); // Para valor2
+
 private final Random rand = new Random();
 
 
 public int leerValore1(String n) {
+    boolean leerPrimeroValor1 = rand.nextBoolean();
 
 boolean leyendoval1 = true;
 rwLock.readLock().lock();
@@ -28,6 +34,11 @@ public void cambiarValor(String n ) {
 	rwLock.writeLock().lock();
 	try {
 		valor1 = round.nextInt(10000);
+		System.out.println("el nuevo valor es " + valor +" y lo ha cambiado " + name);
+		
+	}finally {
+		rwLock.writeLock().unlock()
 	}
 }
+
 }
