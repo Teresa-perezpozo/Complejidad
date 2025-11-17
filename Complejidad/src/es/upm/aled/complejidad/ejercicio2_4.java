7bb1b8b00cebe352ac54e01b5a20c526902d79c5
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-11-17 19:32:24 +0100

    D

--- Complejidad/src/es/upm/aled/complejidad/ejercicio2_4.java
+++ Complejidad/src/es/upm/aled/complejidad/ejercicio2_4.java
@@ -10,6 +10,25 @@ public class ejercicio2_4 {
 	int[] buffer = new int[10] ;
 	int cores = RunTime.getRunTime().avaibleProcessors();
 	ExecutorService executor = 	Executor.newFixedThreadPool(cores);
+	int indexArray =0;
+	String n ;
+	
+	public synchronized void setValue(int value, String n ) {
+		if(indexArray<10){
+			buffer[indexArray]=value;
+			indexArray++;
+			System.out.println(" la hebra " + n +" ha introducido el número " + value);
+		}else {
+		System.out.println(" el array está lleno ");
+		}
+	}
+		public synchronized int leerValue(String n ) {
+			
+			return buffer[indexArray];
+			System.out.println(" la hebra " + n +" ha leido y borrado el número " + buffer[indexArray]);
+			buffer[indexArray]=[];
+			
+		}
 	
 	
 	
