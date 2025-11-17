8d028adb0af83568c9cfcfefa4ed7cd702047b1d
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-11-17 18:15:59 +0100

    d

--- Complejidad/src/es/upm/aled/complejidad/GestorThreads2_3.java
+++ Complejidad/src/es/upm/aled/complejidad/GestorThreads2_3.java
@@ -1,5 +1,19 @@
 package es.upm.aled.complejidad;
 
+import java.util.*;
+import java.util.concurrent.locks.*;
+import java.util.stream.Collectors;
+
 public class GestorThreads2_3 {
 
+	private int valor =0;
+	private final Lock accessLock = new ReentrantLock();
+	//condición para esperar
+	private final Condition waitingCondition = accessLock.newCondition();
+	
+	//estados de la máquina de estados
+	private boolean isWriting = false;
+	private int activeReaders =0;
+	
+	
 }
