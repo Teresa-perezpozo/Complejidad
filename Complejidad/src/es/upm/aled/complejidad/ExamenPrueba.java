6a6fb1119a44b23457d755ae33099800a3a80633
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-11-26 20:04:56 +0100

    dfs

--- Complejidad/src/es/upm/aled/complejidad/ExamenPrueba.java
+++ Complejidad/src/es/upm/aled/complejidad/ExamenPrueba.java
@@ -18,4 +18,49 @@ public class ExamenPrueba {
 
 	    return total;
 	}
+	
+	//ejercicio 3
+	public synchronized void solicitarImpresion(boolean esProfesor) {
+	    try {
+	        while (impresoraEnUso || (esProfesor && (profesoresEsperando >= 2))) {
+	            if (esProfesor) profesoresEsperando++;
+	            wait();
+	        }
+
+	        if (esProfesor) profesoresEsperando--;
+	        impresoraEnUso = true;
+
+	    } catch (InterruptedException e) {}
+	}
+
+	public synchronized void liberarImpresora() {
+	    try {
+	        impresoraEnUso = false;
+	        notifyAll();
+	    } catch (Exception e) {}
+	}public class Estudiante extends Thread {
+	    private Sala Impresion sala;
+
+	    public Estudiante(Sala sala, Impresion sala) { 
+	        this.sala = sala; 
+	    }
+
+	    public void run() {
+	        try {
+	            while (true) {
+	                int eleccion = Random.Math(); // * int eleccion: Random.Math();
+	                boolean esProfesor = eleccion > 0.5;
+
+	                sala.solicitarImpresion(esProfesor);
+
+	                sala.imprimir();
+
+	                sala.liberarImpresora();
+
+	            }
+	        } catch (InterruptedException e) {
+	            Thread.currentThread().interrupt();
+	        }
+	    }
+	}
 }
